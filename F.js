2a981f0277dcb70deb206b69cde0eb12dca92df7
faithadelokun2021 <faithadelokun2021@gmail.com>
2023-10-05 00:51:27 +0100

    initial

--- F.js
+++ F.js
@@ -1,11 +1,11 @@
 // Swiper
     var swiper = new Swiper(".home", {
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+      // spaceBetween: 30,
+      // centeredSlides: true,
+      // autoplay: {
+      //   delay: 2500,
+      //   disableOnInteraction: false,
+      // },
 
       navigation: {
         nextEl: ".swiper-button-next",
